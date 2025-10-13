15bf24dd5477382b6bdbd506d9aa2df60fb73839
channangan <channangan148@gmail.com>
2025-10-13 14:38:51 +0700

    new clean first commit

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router } from "react-router-dom";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import "./index.css";
+import App from "./App.tsx";
+import { store } from "./app/store.ts";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
+  </StrictMode>
+);
