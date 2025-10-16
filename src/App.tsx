c78e84fbda1fbbe423a7f01f0fe421fa0ca319bf
channangan <channangan148@gmail.com>
2025-10-16 16:11:19 +0700

    set the routes for app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 import Footer from "./components/Footer";
 import { Navbar } from "./components/Navbar";
 import { ThemeProvider } from "./components/themeProvider";
+import { Routes, Route } from "react-router-dom";
+import { Home, About, Contact, Accessories, Product } from "./pages/index.ts";
 
 const App = () => {
   return (
     <ThemeProvider defaultTheme="system" storageKey="vite-ui-provider">
-      <Navbar />
-      <Footer />
+      <header>
+        <Navbar />
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/accessories" element={<Accessories />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
+      <footer>
+        <Footer />
+      </footer>
     </ThemeProvider>
   );
 };
---
+++ src/pages/index.ts
@@ -0,0 +1,21 @@
+import About from "./About";
+import Accessories from "./Accessories";
+import Cart from "./Cart";
+import Home from "./Home";
+import Product from "./Product";
+import ProductDetail from "./ProductDetail";
+import Contact from "./Contact";
+import Categaries from "./Categaries";
+import Search from "./Search";
+
+export {
+  About,
+  Accessories,
+  Cart,
+  Product,
+  ProductDetail,
+  Home,
+  Contact,
+  Categaries,
+  Search,
+};
