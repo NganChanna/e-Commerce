0adf42cc12ccbcaf5a1c662e80806606c41a7217
channangan <channangan148@gmail.com>
2025-10-17 15:54:42 +0700

    create tyoe for product and accessory.

---
+++ src/types/Accessory.ts
@@ -0,0 +1,18 @@
+export interface Accessory {
+  id: number;
+  name: string;
+  brand: string;
+  type: string;
+  color: string;
+  price: number;
+  stock: number;
+  image: string;
+  description: string;
+  warranty: string;
+  compatibility: string[];
+  rating: number;
+  review: number;
+  features: string[];
+  weight: string;
+  dimensions: string;
+}
---
+++ src/types/Product.ts
@@ -0,0 +1,18 @@
+export interface Product {
+  id: number;
+  name: string;
+  brand: string;
+  price: number;
+  stock: number;
+  ram: string;
+  storage: string;
+  image: string;
+  descriptoin: string;
+  warranty: string;
+  compatibility: string;
+  rating: number;
+  review: number;
+  feature: string[];
+  weight: string;
+  dimensions: string;
+}
