f8ad81bedde98c7000ac25da9d163b446c0c296d
channangan <channangan148@gmail.com>
2025-11-07 15:57:38 +0700

    udpate url google maps

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -379,7 +379,7 @@ const Contact: React.FC = () => {
           </h2>
           <div className="bg-white dark:bg-gray-800 rounded-3xl overflow-hidden shadow-lg">
             <iframe
-              src="https://maps.app.goo.gl/fX8y41bsCkj8aZjK9"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3168.239034056955!2d-122.084249684681!3d37.42199977982588!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808fba02425dad8d%3A0x6c296c66619367e0!2sGoogleplex!5e0!3m2!1sen!2sus!4v1635781234567!5m2!1sen!2sus"
               width="100%"
               height="400"
               style={{ border: 0 }}
